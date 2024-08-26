8da43ab9230afd8528f90372b9c4b80ab2d7a7dc
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-08-26 11:32:56 +0600

    admin home page desing and responsive done

--- src/Components/Pages/AdminPages/AdminHome.jsx
+++ src/Components/Pages/AdminPages/AdminHome.jsx
@@ -4,10 +4,27 @@ import { NavLink } from 'react-router-dom';
 
 const AdminHome = () => {
     return (
-        <>
+        <div className='h-screen'>
 
-            this is admin home
-        </>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+                <div className='w-[400px] p-5 border-2 rounded '>
+                    <h2 className='text-5xl font-merriweather font-bold text-center text-amber-400'>Total Mobile</h2>
+                    <h3 className='text-5xl font-merriweather font-extrabold text-center text-white'>150 Pcs</h3>
+                </div>
+                <div className='w-[400px] p-5 border-2 rounded '>
+                    <h2 className='text-5xl font-merriweather font-bold text-center text-amber-400'>Total Laptop</h2>
+                    <h3 className='text-5xl font-merriweather font-extrabold text-center text-white'>150 Pcs</h3>
+                </div>
+                <div className='w-[400px] p-5 border-2 rounded '>
+                    <h2 className='text-5xl font-merriweather font-bold text-center text-amber-400'>Total Watch</h2>
+                    <h3 className='text-5xl font-merriweather font-extrabold text-center text-white'>150 Pcs</h3>
+                </div>
+                <div className='w-[400px] p-5 border-2 rounded '>
+                    <h2 className='text-5xl font-merriweather font-bold text-center text-amber-400'>Total Uses</h2>
+                    <h3 className='text-5xl font-merriweather font-extrabold text-center text-white'>150 Pcs</h3>
+                </div>
+            </div>
+        </div>
     );
 };
 
